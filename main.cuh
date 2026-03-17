3a91497249811683618a370574c02599eea10282
Elias D <eliasdandouch@gmail.com>
2026-03-16 17:17:30 -0700

    Delete main.cuh

--- main.cuh
+++
@@ -1,9 +0,0 @@
-#include "vector_addition.cuh"
-#include "matrix_multiplication.cuh"
-#include <cuda_runtime.h>
-#include <iostream>
-#include <vector>
-
-int main() {
-    std::cout << "CUDA project with multiple files\n";
-}
